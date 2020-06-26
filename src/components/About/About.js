09b0d43c78a62481dcc48d9f92503c2bd2d97ffb
alnasimul <alnasimul@gmail.com>
2020-06-27 00:39:01 +0600

    about section basic setup updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import './About.css';
+import img1 from '../../Image/adult-blur-blurred-background-687824.png';
+import img2 from '../../Image/chef-cook-food-33614.png';
+import img3 from '../../Image/architecture-building-city-2047397.png';
+import icon1 from '../../ICON/Group 204.png';
+import icon2 from '../../ICON/Group 1133.png';
+import icon3 from '../../ICON/Group 245.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
+
 
 const About = () => {
     return (
@@ -10,7 +19,51 @@ const About = () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, obcaecati odit.</p>
                 </div>
                 <div className="about-bottom">
-                    
+                    <div className="quarter-width">
+                        <div>
+                            <img src={img1} alt=""/>
+                        </div>
+                        <div>
+                            <div className="feature-top">
+                                <img src={icon1} alt=""/>
+                                <h4>Fast Delivery</h4>
+                            </div>
+                            <div className="feature-bottom">
+                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus illum ducimus totam minus.</p>
+                                 <a href="#">See more <FontAwesomeIcon className="arrow" icon={faArrowAltCircleRight} /> </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="quarter-width special-height">
+                        <div>
+                            <img src={img2} alt=""/>
+                        </div>
+                        <div>
+                            <div className="feature-top">
+                                <img src={icon2} alt=""/>
+                                <h4>A Good Auto Responder </h4>
+                            </div>
+                            <div className="feature-bottom">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus illum ducimus totam minus.</p>
+                                <a href="#">See more <FontAwesomeIcon className="arrow" icon={faArrowAltCircleRight} /> </a> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="quarter-width">
+                        <div>
+                            <img src={img3} alt=""/>
+                        </div>
+                        <div>
+                            <div className="feature-top">
+                                <img src={icon3} alt=""/>
+                                <h4>Home Delivery</h4>
+                            </div>
+                            <div className="feature-bottom">
+                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus illum ducimus totam minus.</p>
+                                 <a href="#">See more <FontAwesomeIcon className="arrow" icon={faArrowAltCircleRight} /> </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
