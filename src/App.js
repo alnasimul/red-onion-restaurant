d5beeee1c13e91fe4bdcd1d9e1db2ce8ac56746a
alnasimul <alnasimul@gmail.com>
2020-07-07 23:19:11 +0600

    basic routing added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
 import Shop from './components/Shop/Shop';
 import About from './components/About/About';
 import Footer from './components/Footer/Footer';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Banner></Banner>
-      <Shop></Shop>
-      <About></About>
-      <Footer></Footer>
+      <Router>
+          <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Banner></Banner>
+            <Shop></Shop>
+            <About></About>
+          </Route>
+          <Route path="/shop">
+            <Banner></Banner>
+            <Shop></Shop>
+            <About></About>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+          <Footer></Footer>
+      </Router>
     </div>
   );
 }
