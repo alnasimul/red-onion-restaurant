6b9304bb477072bafa084f21eb0ee18cda6c1ffd
Al Nasimul Haque <alnasimul@gmail.com>
2020-10-19 21:54:56 +0600

    cartItems.js code added

--- src/components/CartItems/CartItems.js
+++ src/components/CartItems/CartItems.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
 
-const CartItems = () => {
+const CartItems = (props) => {
+
+    const {name,price,quantity,image,id} = props.currentFood;
+    const [quantityCount,setQuantityCount] = useState(quantity);
+
+    const handelAdd = clickedId => {
+        const foods = fakeData;
+        console.log(foods)
+       // const currentFood = fakeData.find()
+    }
     return (
         <div>
             
